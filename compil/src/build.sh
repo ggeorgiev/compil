9d617647022832143ebe25cef206e2601aecf766
george <george.georgiev@hotmail.com>
2013-01-03 22:58:12 -0800

    start using the project generation

--- compil/src/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-../compil-stable/tool/macosxx86/bin/generator --project-file=language/language.scompilprj -o ../../intermediate/test || exit 1
-../boost/tool/macosxx86/bin/b2 toolset=darwin variant=debug link=static architecture=x86 address-model=64 -j1 --hash || exit 1
