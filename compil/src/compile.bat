0b131b53135634f0cf28d6a65a241588f5b0e53d
george <george.georgiev@hotmail.com>
2013-01-03 23:47:49 -0800

    add generator call to the compil.bat

--- compil/src/compile.bat
+++ compil/src/compile.bat
@@ -1,1 +1,2 @@
+..\compil-stable\tool\windowsx86\bin\generator --project-file=language\language.scompilprj -o language\.gen || exit 1
 ..\boost\tool\windowsx86\bin\b2.exe toolset=msvc-10.0 variant=debug address-model=32 architecture=x86 link=static runtime-link=static -j8
