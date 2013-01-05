50f165f5132a5d8fc39498c5b348187f46897a1f
unknown <Administrator@winxp64ci.(none)>
2013-01-05 02:17:57 -0800

    add get-latest generators on windows

---
+++ compil/compil-stable/get-latest-generators.bat
@@ -0,0 +1,10 @@
+SET BASEDIR=%~dp0
+echo %BASEDIR%
+
+pushd %BASEDIR% || exit 1
+
+curl -o tool\macosxx86\bin\generator "http://192.168.1.73:8080/view/compil/job/compil-deploy-macosx/lastSuccessfulBuild/artifact/intermediate/generator/darwin-4.2.1/release/address-model-64/architecture-x86/link-static/threading-multi/generator" || exit 1
+
+curl -o tool\windowsx86\bin\generator.exe "http://192.168.1.73:8080/view/compil/job/compil-deploy-windows/lastSuccessfulBuild/artifact/intermediate/generator/672bee5f36160e5defbaeb652e109dfa/generator.exe" || exit 1
+
+popd || exit 1
