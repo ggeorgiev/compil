3ce6e1faa79c0dec5c51130564db25a1483a7097
george <george.georgiev@hotmail.com>
2013-01-05 20:10:48 -0800

    update the get latest scripts

--- compil/compil-stable/get-latest-generators.bat
+++ compil/compil-stable/get-latest-generators.bat
@@ -4,7 +4,7 @@ echo %BASEDIR%
 pushd %BASEDIR% || exit 1
 
 curl -o tool\macosxx86\bin\generator "http://192.168.1.73:8080/view/compil/job/compil-deploy-macosx/lastSuccessfulBuild/artifact/intermediate/generator/darwin-4.2.1/release/address-model-64/architecture-x86/link-static/threading-multi/generator" || exit 1
-
+curl -o tool\linuxx86\bin\generator "http://192.168.1.73:8080/view/compil/job/compil-deploy-linux/lastSuccessfulBuild/artifact/intermediate/generator/gcc-4.7/release/address-model-64/architecture-x86/link-static/threading-multi/generator" || exit 1
 curl -o tool\windowsx86\bin\generator.exe "http://192.168.1.73:8080/view/compil/job/compil-deploy-windows/lastSuccessfulBuild/artifact/intermediate/generator/672bee5f36160e5defbaeb652e109dfa/generator.exe" || exit 1
 
 popd || exit 1
