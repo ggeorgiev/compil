be992e66aae17244e9cd2f72da73e6f1ee1452f8
george <george.georgiev@hotmail.com>
2013-01-10 18:56:55 -0800

    add generate script for macosx

---
+++ compil/scripts/macosx/generate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+BASEDIR=$(dirname $0)
+
+pushd $BASEDIR/../../src || exit 1
+
+../compil-stable/tool/macosxx86/bin/generator \
+    --project-file=language/language.scompilprj \
+    --output-directory=language/.gen \
+    --core-output-directory=. \
+    --cpp.include_path=include_path_based_on_package \
+    || exit 1
+
+popd || exit 1
